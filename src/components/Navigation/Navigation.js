744a8b1fac5597f659155d17e83b135368462e6b
Javier Alaves <jayalaves@gmail.com>
2023-10-14 16:41:14 +0200

    sign out button style

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navigation = () => {
 	return (
-		<nav className='flex justify-end text-lg min-w-full text-black underline p-3 cursor-pointer transition hover:opacity-80'>
+		<nav className='flex justify-end text-lg min-w-full text-slate-700 font-medium p-4 cursor-pointer transition hover:opacity-60'>
 			Sign Out
 		</nav>
 	);
