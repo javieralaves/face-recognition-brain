fe98385436e21b52139a9c6076a2ae67715ecb2e
Javier Alaves <jayalaves@gmail.com>
2023-10-14 15:52:32 +0200

    logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import person from './person.png';
+
+const Logo = () => {
+	return (
+		<div className='m-4 mt-0 h-40 w-40'>
+			<img src={person} alt='' />
+		</div>
+	);
+};
+
+export default Logo;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Navigation = () => {
-	return <nav className='flex justify-end'>Sign Out</nav>;
+	return (
+		<nav className='flex justify-end text-lg text-black underline p-3 cursor-pointer transition hover:opacity-80'>
+			Sign Out
+		</nav>
+	);
 };
 
 export default Navigation;
