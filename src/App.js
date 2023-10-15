b52db99edb7709c1c6405dae42daa06f99eaad46
Javier Alaves <jayalaves@gmail.com>
2023-10-15 09:45:07 +0200

    basic form functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
 
 function App() {
+	const [imageInput, setImageInput] = useState();
+
+	const onSubmit = () => {
+		console.log('click');
+	};
+
 	return (
 		<div className='flex flex-col items-center min-h-screen'>
 			<Navigation />
 			<Logo />
-			<ImageLinkForm />
+			<ImageLinkForm onChange={setImageInput} onSubmit={onSubmit} />
 			{/* <FaceRecognition /> */}
 		</div>
 	);
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ setImageInput, onSubmit }) => {
 	return (
 		<div className='text-lg'>
 			<p>
 				{'This magic brain will detect faces in your images. Give it a shot!'}
 			</p>
 			<div className='p-4'>
-				<input type='text' className='w-96 rounded-md p-3' />
-				<button className='rounded-md font-medium py-3 px-4 ml-3 bg-slate-800 text-white transition-all duration-150 hover:bg-slate-900 hover:cursor-pointer'>
+				<input
+					type='text'
+					className='w-96 rounded-md p-3'
+					onChange={setImageInput}
+				/>
+				<button
+					className='rounded-md font-medium py-3 px-4 ml-3 bg-slate-800 text-white transition-all duration-150 hover:bg-slate-900 hover:cursor-pointer'
+					onClick={onSubmit}
+				>
 					Detect
 				</button>
 			</div>
